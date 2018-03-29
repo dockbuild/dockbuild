50fc3a397fbab75932cc83db6f1dd6aec7cb77fa
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-03-29 17:35:16 -0400

    install-gosu-binary: Add support for 32/64 arch, use temporary GNUPGHOME
    Also verify that the binary works calling "gosu nobody true"
    
    See https://github.com/tianon/gosu/blob/master/INSTALL.md

--- imagefiles/install-gosu-binary.sh
+++ imagefiles/install-gosu-binary.sh
@@ -13,20 +13,27 @@ if ! command -v gpg &> /dev/null; then
 fi
 
 GOSU_VERSION=1.10
+dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')";
+url="https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-${dpkgArch}"
+url_key="https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-${dpkgArch}.asc"
+
+# download and verify the signature
+export GNUPGHOME="$(mktemp -d)"
 
 gpg --keyserver pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
 
-url="https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-amd64"
 echo "Downloading $url"
 curl -o /usr/local/bin/gosu -# -SL $url
 
-url="https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-amd64.asc"
-echo "Downloading $url"
-curl -o /usr/local/bin/gosu.asc -# -SL $url
+echo "Downloading $url_key"
+curl -o /usr/local/bin/gosu.asc -# -SL $url_key
 
 gpg --verify /usr/local/bin/gosu.asc
 
-rm /usr/local/bin/gosu.asc
-rm -r /root/.gnupg/ || echo "/root/.gnupg/ not found"
+# cleanup
+rm -r "$GNUPGHOME" /usr/local/bin/gosu.asc
 
 chmod +x /usr/local/bin/gosu
+
+# verify that the binary works
+gosu nobody true
