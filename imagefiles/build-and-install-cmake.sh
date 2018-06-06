2ab4b45aa0dce3be927b484aa57fc5fcf92ded56
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-06-06 19:43:28 -0400

    Tweak install scripts to match version using in dockcross
    * fix intermittent CMake build error using two separate build directories.
      One for bootstrap CMake, and one for CMake with SSL support.
    
    * improve reliability of install-gosu-binary.sh script attempting to download
      GPG key from  multiple server.

--- imagefiles/build-and-install-cmake.sh
+++ imagefiles/build-and-install-cmake.sh
@@ -29,35 +29,33 @@ fi
 
 cd /usr/src
 
-git clone git://cmake.org/cmake.git CMake
-
-(cd CMake && git checkout v$CMAKE_VERSION)
+git clone git://cmake.org/cmake.git CMake -b v$CMAKE_VERSION --depth 1
 
 mkdir /usr/src/CMake-build
 cd /usr/src/CMake-build
 
 ${WRAPPER} /usr/src/CMake/bootstrap \
-  --parallel=$(grep -c processor /proc/cpuinfo) \
-  --prefix=/usr/src/CMake-$CMAKE_VERSION
+  --parallel=$(grep -c processor /proc/cpuinfo)
 ${WRAPPER} make -j$(grep -c processor /proc/cpuinfo)
 
-${WRAPPER} ./bin/cmake \
+mkdir /usr/src/CMake-ssl-build
+cd /usr/src/CMake-ssl-build
+
+${WRAPPER} /usr/src/CMake-build/bin/cmake \
   -DCMAKE_BUILD_TYPE:STRING=Release \
   -DBUILD_TESTING:BOOL=OFF \
+  -DCMAKE_INSTALL_PREFIX:PATH=/usr/src/cmake-$CMAKE_VERSION \
   -DCMAKE_USE_OPENSSL:BOOL=ON \
   -DOPENSSL_ROOT_DIR:PATH=/usr/local/ssl \
-  .
+  ../CMake
 ${WRAPPER} make -j$(grep -c processor /proc/cpuinfo) install
 
 # Cleanup install tree
-cd /usr/src/CMake-$CMAKE_VERSION
+cd /usr/src/cmake-$CMAKE_VERSION
 rm -rf doc man
 
 # Install files
 find . -type f -exec install -D "{}" "/usr/{}" \;
-
-# Test
-ctest -R CMake.FileDownload
 
 # Write test script
 cat <<EOF > cmake-test-https-download.cmake
@@ -81,5 +79,5 @@ EOF
 # Execute test script
 cmake -P cmake-test-https-download.cmake
 
-# Remove source and build tree
+# Remove source and build trees
 rm -rf /usr/src/CMake*
