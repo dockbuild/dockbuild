b27fc6692caec36961fbd6e13422b123c32e4ab5
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-06-10 17:25:53 -0400

    Add "UpdateFromUpstream.sh" to sync with dockcross/imagefiles scripts

---
+++ imagefiles/UpdateFromUpstream.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+thirdparty_module_name='imagefiles'
+
+upstream_git_url='git://github.com/dockcross/dockcross'
+upstream_git_branch='master'
+github_compare=true
+
+snapshot_author_name='Dockcross Upstream'
+snapshot_author_email='matt.mccormick@kitware.com'
+
+snapshot_redact_cmd=''
+snapshot_relative_path='.'
+snapshot_paths='
+  imagefiles/build-and-install-curl.sh
+  imagefiles/build-and-install-git.sh
+  imagefiles/build-and-install-openssh.sh
+  imagefiles/build-and-install-openssl.sh
+  imagefiles/dockcross
+  imagefiles/entrypoint.sh
+  imagefiles/install-cmake-binary.sh
+  imagefiles/install-gosu-binary.sh
+  imagefiles/utils.sh
+  '
+
+source "${BASH_SOURCE%/*}/../utilities/maintenance/UpdateThirdPartyFromUpstream.sh"
+update_from_upstream
