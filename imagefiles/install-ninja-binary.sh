4bf9680cbdcff919a20a9b53d043fce07dad052c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2025-08-27 13:43:44 -0400

    chore: Refactor install-ninja-binary to use fetch_source

--- imagefiles/install-ninja-binary.sh
+++ imagefiles/install-ninja-binary.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+# Top-level build script called from Dockerfile
 
 set -ex
 
-if ! command -v curl &> /dev/null; then
-	echo >&2 'error: "curl" not found!'
-	exit 1
-fi
+# Get script directory
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+# Get build utilities
+source $SCRIPT_DIR/utils.sh
 
 if ! command -v tar &> /dev/null; then
-	echo >&2 'error: "tar" not found!'
-	exit 1
+  echo >&2 'error: "tar" not found!'
+  exit 1
 fi
 
 if [[ "${NINJA_VERSION}" == "" ]]; then
@@ -19,16 +21,24 @@ fi
 
 cd /usr/src
 
-#url="https://github.com/ninja-build/ninja/releases/download/v${NINJA_VERSION}/ninja-linux.zip"
-url="https://github.com/Kitware/ninja/releases/download/v${NINJA_VERSION}/ninja-${NINJA_VERSION}_x86_64-linux-gnu.tar.gz"
+# Download
+#NINJA_DOWNLOAD_URL="https://github.com/ninja-build/ninja/releases/download/v${NINJA_VERSION}/"
+#NINJA_ROOT="ninja-linux"
+#NINJA_ARCHIVE="${NINJA_ROOT}.zip"
+NINJA_DOWNLOAD_URL="https://github.com/Kitware/ninja/releases/download/v${NINJA_VERSION}"
+NINJA_ROOT="ninja-${NINJA_VERSION}_x86_64-linux-gnu"
+NINJA_ARCHIVE="${NINJA_ROOT}.tar.gz"
 
-echo "Downloading $url"
-curl -# -LO $url
+echo "Downloading ${NINJA_DOWNLOAD_URL}/${NINJA_ARCHIVE}"
+fetch_source "${NINJA_ARCHIVE}" "${NINJA_DOWNLOAD_URL}"
 
-#unzip ninja-linux.zip
-#rm -f ninja-linux.zip
-tar --strip-components 1 -xzvf ninja-${NINJA_VERSION}_x86_64-linux-gnu.tar.gz
-rm -f ninja-${NINJA_VERSION}_x86_64-linux-gnu.tar.gz
-
+# Since the archive name may not match the top-level directory, explicitly specify it.
+mkdir -p "${NINJA_ROOT}"
+tar xvzf "${NINJA_ARCHIVE}" --strip-components=1 -C "${NINJA_ROOT}"
+pushd "${NINJA_ROOT}"
 mv ninja /usr/local/bin/
+popd
+rm -rf "${NINJA_ROOT}" "${NINJA_ARCHIVE}"
 
+hash -r
+ninja --version
